52db0eec598ae7bbce7beb11c04821a85358de68
voidSatisfaction <lourie@naver.com>
2020-04-27 00:00:30 +0900

    test: sync test

--- pkg/mailManager/mailManager_test.go
+++ pkg/mailManager/mailManager_test.go
@@ -7,7 +7,7 @@ import (
 func TestMailManagerSend(t *testing.T) {
 
 	mm := New("test@gmail.com", "password", "smtp.gmail.com", "587")
-	err := mm.Send("test@gmail.com", []string{"test@gmail.com"}, "hello world!")
+	err := mm.SendHTML("test@gmail.com", []string{"test@gmail.com"}, "subject", "hello world!")
 
 	if err == nil {
 		t.Fatalf("expected err not nil, but got nil")
