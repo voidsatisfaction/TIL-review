717bd72095777d4531987d1ae6282462ac36ba24
yeongyumin <yeongyumin@vuno.co>
2020-05-24 11:04:48 +0900

    chore: change ci setting and change dockerfile for production friendly

--- script/production/run_everyday_review.sh
+++ script/production/run_everyday_review.sh
@@ -1,2 +1,1 @@
-# It uses absolute path as this script is called by crontab
-sudo docker run --rm -v /home/ubuntu/TIL-review:/go/src/til-review-app til-review:1.0.0 go run cmd/everydayReview.go cmd/config.json template/everyday_review.html.tmpl
+sudo docker run --rm til-review:1.0.0 go run cmd/everydayReview.go cmd/config.json template/everyday_review.html.tmpl
